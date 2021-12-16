7e0c32cecb0c23e4223fc70c7943f475c4a9f45d
Robert <92167234+17RPakulski@users.noreply.github.com>
2021-12-16 12:36:02 +0000

    Add files via upload
    16 dec

--- 15 coding exam revision.py
+++ 15 coding exam revision.py
@@ -8,7 +8,6 @@ given_list = [23,1,0,-12,48]
 add_1 = 0
 for item in given_list:
     add_1 += item
-    
 
 file_write.write(str(add_1) +'\n')
 
@@ -30,10 +29,8 @@ for item in given_list:
     if item % 2 == 0:
         add_3 += 1
 
-file_write.write(str(add_3))
+file_write.write(str(add_3) + '\n')
 
-
-file_write.close()
 file_read = open("RobertChristmasTest2021.txt", "r")
 for line in file_read:
     print(line)
@@ -41,10 +38,52 @@ for line in file_read:
 
 
 
-# 4    WRONG
+# 4  
+multiply = 1
+for item in given_list:
+    multiply = multiply * item
 
-    
-    
+file_write.write(str(multiply) + '\n')
+
+
+
+
+# 5
+for i in range(0,21,4):
+    file_write.write(str(i) + ' ')
+
+
+
+
+'''
+print('gap')
+file_write.close()
+for line in file_read:
+    print(line)
+'''
+
+
+
+
+
+
+
+
+
+
+
+# While loop
+print('while loop start')
+
+
+# 6
+counter = 0
+input_string = 'cat'
+length_of_string = (len((input_string)))
+
+while counter < length_of_string:
+        print(input_string[counter])
+        counter += 1
 
 
 
